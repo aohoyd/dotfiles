1e7c69505c5eebd851bdd6fee3f6a06bc8f7d34b
Aleksei Olshanskiy <a.v.olshanskiy@tinkoff.ru>
2022-02-01 12:11:48 +0300

    Update

--- dot_config/custom/main.fish
+++ dot_config/custom/main.fish
@@ -1,6 +1,7 @@
 #!/usr/bin/env fish
 
 source (brew --prefix asdf)/asdf.fish
+source (dirname (status --current-filename))/kube-alias.fish
 
 function atttmux
     set -l sName local
@@ -12,16 +13,13 @@ function atttmux
     end
 end
 
-abbr -a slab ssh util
-abbr -a spro ssh util001.scl.five9.com
-abbr -a xlab xxh util
-abbr -a xpro xxh util001.scl.five9.com
-
 set TERM xterm-256color
 set EDITOR /usr/local/bin/emacs
+set DOCKER_HOST unix:///Users/a.v.olshanskiy/.lima/docker/sock/docker.sock
 
 set LC_ALL en_US.UTF-8
 
 set PATH $PATH ~/go/bin
 
 set NNN_PLUG 'f:finder;o:fzopen;c:fzcd'
+
