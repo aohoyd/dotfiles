e6c68f847ac831366cf675cca6dc8a1a228e54d6
Aleksei Olshanskiy <aolshanskiy@ptsecurity.com>
2022-10-26 13:53:24 +0300

    Update

--- dot_config/custom/main.fish
+++ dot_config/custom/main.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-source (brew --prefix asdf)/asdf.fish
+source /run/current-system/sw/share/fish/vendor_completions.d/asdf.fish
 source (dirname (status --current-filename))/kube-vars.fish
 source (dirname (status --current-filename))/kube-alias.fish
 
@@ -17,11 +17,13 @@ end
 set -x TERM xterm-256color
 set -x EDITOR /usr/local/bin/emacsclient -t
 set -x VISUAL /usr/local/bin/emacsclient -c
-set -x DOCKER_HOST 'unix:///Users/a.v.olshanskiy/.local/share/containers/podman/machine/podman-machine-default/podman.sock'
+set -x DOCKER_HOST 'unix:///Users/avlllo/.local/share/containers/podman/machine/podman-machine-default/podman.sock'
+
+set -x GOPRIVATE gitlab.ptsecurity.com
 
 set -x LC_ALL en_US.UTF-8
 
-set -x PATH $PATH ~/go/bin ~/.krew/bin "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
+set -x PATH $PATH ~/go/bin ~/.krew/bin ~/.asdf/shims
 
 set -x NNN_PLUG 'f:finder;o:fzopen;c:fzcd'
 
