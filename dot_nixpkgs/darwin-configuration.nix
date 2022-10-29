03bfeb6dfd6d93004fb1bee1e09bc34e4f1028cd
Aleksei Olshanskiy <aolshanskiy@ptsecurity.com>
2022-10-29 14:37:02 +0300

    Add options

--- dot_nixpkgs/darwin-configuration.nix
+++ dot_nixpkgs/darwin-configuration.nix
@@ -1,10 +1,13 @@
 { config, pkgs, ... }:
 
 {
+  imports = [ <home-manager/nix-darwin> ];
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages =
-    [ pkgs.vim
+    [ pkgs.kitty
+      pkgs.vim
       pkgs.fish
       pkgs.chezmoi
       pkgs.zellij
@@ -22,11 +25,14 @@
       pkgs.qemu
       pkgs.docker-client
       pkgs.asdf-vm
+      pkgs.go_1_19
+      pkgs.kind
+      pkgs.minikube
     ];
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+  environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
@@ -37,9 +43,22 @@
   '';
 
   # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
-  programs.fish.enable = true;
+  security.pam.enableSudoTouchIdAuth = true;
 
+  programs = {
+    fish.enable = true;
+  };
+
+  environment.shells = [ pkgs.fish ];
+
+  users.users.schickling = {
+    home = "/Users/schickling";
+    shell = "${pkgs.fish}/bin/fish";
+  };
+  users.users.root = {
+    home = "/var/root";
+    shell = "${pkgs.fish}/bin/fish";
+  };
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
