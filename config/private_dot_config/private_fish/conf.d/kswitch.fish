99588995e5eaac46637feee7112383d6a358171b
Alexey Olshanskiy <gh@aohoy.dev>
2025-06-22 19:16:22 +0300

    Sync config

--- config/private_dot_config/private_fish/conf.d/kswitch.fish
+++
@@ -1,5 +0,0 @@
-# fisher initialization, protected as omf also tries to run it.
-set -q fisher_path; or set -l fisher_path $__fish_config_dir
-if test -f $fisher_path/functions/kswitch.fish
-    source $fisher_path/functions/kswitch.fish
-end
